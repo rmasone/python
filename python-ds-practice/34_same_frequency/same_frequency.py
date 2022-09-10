4fc50f7e805b87b2ded762ecb9596323b23c5291
Richard Masone <richardmasone@outlook.com>
2022-09-10 10:52:52 -0400

    add completed python assignments

---
+++ python-ds-practice/34_same_frequency/same_frequency.py
@@ -0,0 +1,22 @@
+def freq_count(num):
+    counter={}
+
+    for x in num:
+        counter[x]=counter.get(x,0)+1
+
+    return counter
+
+
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+    
+        >>> same_frequency(551122, 221515)
+        True
+        
+        >>> same_frequency(321142, 3212215)
+        False
+        
+        >>> same_frequency(1212, 2211)
+        True
+    """
+    return freq_count(str(num1)) == freq_count(str(num2))
---
+++ python-ds-practice/fs_2_valid_parentheses/valid_parentheses.py
@@ -0,0 +1,34 @@
+def valid_parentheses(parens):
+    """Are the parentheses validly balanced?
+
+        >>> valid_parentheses("()")
+        True
+
+        >>> valid_parentheses("()()")
+        True
+
+        >>> valid_parentheses("(()())")
+        True
+
+        >>> valid_parentheses(")()")
+        False
+
+        >>> valid_parentheses("())")
+        False
+
+        >>> valid_parentheses("((())")
+        False
+
+        >>> valid_parentheses(")()(")
+        False
+    """
+    count = 0
+
+    for char in parens:
+        if char == "(":
+            char += 1
+        elif char == ")":
+            char -= 1
+        
+    return count == 0
+
